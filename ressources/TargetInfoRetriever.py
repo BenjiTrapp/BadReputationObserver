556bc0ec4935062ea04499463aaa0c2dc2caf776
Nyctophobia <nyctophobia@protonmail.ch>
2016-12-21 19:02:36 +0100

    Itty-Bitty Refactoring and cleaned up the target info output mess

--- ressources/TargetInfoRetriever.py
+++ ressources/TargetInfoRetriever.py
@@ -2,30 +2,30 @@ import logging
 import re
 import requests
 
-
-
 logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 class TargetInfoRetriever(object):
+    __NEW_LINE = '\n'
+    __CARRIAGE_RET = '\\r'
+    __PADDING_BLANKS = "   "
+    __REGEX_CHECK_IF_IP_IS_VALID = r'^((\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])$'
+
     def __init__(self):
             self.__BAD_IP = None
-            self.__NEW_LINE = '\n'
+
 
     def retrieve_target_information(self, ip):
         global tracert, nmap, geoip, reversed_dns, http_headers
-        __REGEX_CHECK_IF_IP_IS_VALID = r'^((\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])$'
 
         if not ip or ip is None:
             self.__BAD_IP = requests.get('http://myexternalip.com/raw')
         else:
             self.__BAD_IP = ip
 
-        if not re.match(__REGEX_CHECK_IF_IP_IS_VALID, str(self.__BAD_IP)):
+        if not re.match(self.__REGEX_CHECK_IF_IP_IS_VALID, str(self.__BAD_IP)):
             return "The passed ip: " + str(ip) + " is malformed. Currently only valid IPV4 Addresses are allowed!"
-
-        print("BadIp: " + str(self.__BAD_IP))
 
         try:
             reversed_dns = requests.get('http://api.hackertarget.com/reverseiplookup/?q=' + str(self.__BAD_IP), stream = True).text
@@ -42,10 +42,10 @@ class TargetInfoRetriever(object):
 
         return self.__get_target_info_as_dictionary()
 
-    # TODO: Prettify the output
     def __get_target_info_as_dictionary(self):
         return {"Reverse DNS":  str(reversed_dns.split(self.__NEW_LINE)),
                 "GeoIP":        str(geoip.split(self.__NEW_LINE)),
                 "NMAP":         str(nmap.split(self.__NEW_LINE)),
-                "Trace Route":  str(tracert.split(self.__NEW_LINE)),
-                "HTTP Headers": str(http_headers.split(self.__NEW_LINE))}
+                "HTTP Headers": str(http_headers.split(self.__NEW_LINE)).replace(self.__CARRIAGE_RET, ''),
+                "Trace Route":  str(tracert.split(self.__NEW_LINE)).replace(self.__PADDING_BLANKS, '')
+                }
