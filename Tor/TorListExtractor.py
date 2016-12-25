94363a96042c9bcdb382f4abe390674d4a2da09e
Nyctophobia <nyctophobia@protonmail.ch>
2016-12-25 15:43:52 +0100

    Tiny refactoring of the tor list endpoint

--- Tor/TorListExtractor.py
+++ Tor/TorListExtractor.py
@@ -22,11 +22,9 @@ class TorListExtractor:
             self.__download_tor_exit_nodes()
             logging.info("Done. All TOR-Lists refreshed")
 
-    def check_if_ip_is_exit_node(self, ip):
-        return self.__is_ip_in_tor_list(ip, self.__TOR_EXIT_NODES_FILE_NAME)
-
-    def check_if_ip_is_tor_node(self, ip):
-        return self.__is_ip_in_tor_list(ip, self.__TOR_ALL_NODES_FILE_NAME)
+    def check_tor_node(self, ip):
+        return [{"TOR-Exit-Node": self.__is_ip_in_tor_list(ip, self.__TOR_EXIT_NODES_FILE_NAME)},
+                {"TOR-Node:": self.__is_ip_in_tor_list(ip, self.__TOR_ALL_NODES_FILE_NAME)}]
 
     def __download_tor_exit_nodes(self):
         if not self.save_list_from_url(self.__TOR_EXIT_NODES_LIST_URL, self.__TOR_EXIT_NODES_FILE_NAME):
@@ -42,19 +40,13 @@ class TorListExtractor:
         if not re.match(self.__REGEX_CHECK_IF_IP_VALID, str(ip)):
             return "The passed ip: " + str(ip) + " is malformed. Currently only valid IPV4 Addresses are allowed!"
 
-        if filename is self.__TOR_EXIT_NODES_FILE_NAME:
-            result = "TOR-Exit-Node: "
-        elif filename is self.__TOR_ALL_NODES_FILE_NAME:
-            result = "TOR-Node: "
-        else:
-            logging.error("Filename: " + filename + " is not supported!")
-            raise FileNotFoundError("The given filename wasn't recognized")
+        assert filename is self.__TOR_ALL_NODES_FILE_NAME or filename is self.__TOR_EXIT_NODES_FILE_NAME
 
         with open(filename) as f:
             for line in f:
                 if ip in line:
-                    return result + "Yes"
-        return result + "No"
+                    return "Yes"
+        return "No"
 
     @staticmethod
     def save_list_from_url(url = None, filename = None):
@@ -70,6 +62,3 @@ class TorListExtractor:
             shutil.copyfileobj(response, out_file)
 
         return os.path.exists(filename)
-
-if __name__ == '__main__':
-    TorListExtractor(refresh_lists = True)
